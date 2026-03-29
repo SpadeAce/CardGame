17e0e61abf68a3d20e70f4311b3e24c49687875e
Edward Sim <sjbv@naver.com>
2026-03-29 14:02:42 +0900

    convert

--- Assets/Scripts/UI/Info/InfoPopup.cs
+++ Assets/Scripts/UI/Info/InfoPopup.cs
@@ -1,6 +1,36 @@
+using System;
+using System.Collections.Generic;
+using SA;
+using SA.UI;
 using UnityEngine;
 
-public class InfoPopup : MonoBehaviourEx
+[AssetPath("Prefabs/UI/Info/InfoPopup")]
+public class InfoPopup : PopupView
 {
-    
+    public enum ObjectType {NONE, ENEMY, PAWN, CARD, ITEM, EQUIPMENT}
+    #region Inner Class
+    [Serializable]
+    public class InfoPreset
+    {
+        public ObjectType type;
+        public GameObject prefab;
+    }
+    #endregion Inner Class
+
+    #region Links
+    public List<InfoPreset> _infoPresetList = new List<InfoPreset>();
+
+    [Linker("Root")]
+    public GameObject _root;
+    #endregion Links
+
+    private ObjectType currentType = ObjectType.NONE;
+    private BaseInfoPreset currentPreset;
+
+    public void SetData(DObject obj)
+    {
+        ObjectType targetType = ObjectType.NONE;
+        
+
+    }
 }
